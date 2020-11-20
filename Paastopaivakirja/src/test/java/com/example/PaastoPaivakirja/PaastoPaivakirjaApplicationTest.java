11c3160fa8edabb709edb1b697e2ec8e8dadd24d
Oskari-Juvakka <oskari.juvakka@gmail.com>
2020-11-20 15:36:51 +0200

    uudelleennimeys

--- Paastopaivakirja/src/test/java/com/example/PaastoPaivakirja/PaastoPaivakirjaApplicationTest.java
+++ Paastopaivakirja/src/test/java/com/example/PaastoPaivakirja/PaastoPaivakirjaApplicationTest.java
@@ -9,9 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PaastoPaivakirjaApplicationTest {
+public class PaastopaivakirjaApplicationTest {
 
     @Test
     public void contextLoads() {
+    
     }
 }
