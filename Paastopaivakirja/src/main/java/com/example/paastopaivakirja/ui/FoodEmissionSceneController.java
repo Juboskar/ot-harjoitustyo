3871a2adde8b8c101a7332c1e210a9e232a5639e
Oskari-Juvakka <oskari.juvakka@gmail.com>
2020-11-23 13:00:51 +0200

    aloitettu päivittäisten ruoan päästöjen laskemistoiminnallisuus

--- Paastopaivakirja/src/main/java/com/example/paastopaivakirja/ui/FoodEmissionSceneController.java
+++ Paastopaivakirja/src/main/java/com/example/paastopaivakirja/ui/FoodEmissionSceneController.java
@@ -1,5 +1,9 @@
 package com.example.paastopaivakirja.ui;
 
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +14,82 @@ import org.springframework.stereotype.Component;
 @Component
 @FxmlView("/fxml/FoodEmissionScene.fxml")
 public class FoodEmissionSceneController {
+
+    @FXML
+    Slider cowSlider;
+
+    @FXML
+    Slider pigSlider;
+
+    @FXML
+    Slider fishSlider;
+
+    @FXML
+    Slider CheeseSlider;
+
+    @FXML
+    Slider riceSlider;
+
+    @FXML
+    Slider eggSlider;
+    
+    @FXML
+    Slider restaurantSlider;
+
+    @FXML
+    Text cowSliderValue;
+
+    @FXML
+    Text pigSliderValue;
+
+    @FXML
+    Text fishSliderValue;
+
+    @FXML
+    Text cheeseSliderValue;
+
+    @FXML
+    Text riceSliderValue;
+
+    @FXML
+    Text eggSliderValue;
+    
+    @FXML
+    Text restaurantSliderValue;
+
+    @FXML
+    public void setCow(ObservableValue<Number> ovn, Number before, Number after) {
+        cowSliderValue.setText(after.intValue() + " g");
+    }
+
+    @FXML
+    public void setPig(ObservableValue<Number> ovn, Number before, Number after) {
+        pigSliderValue.setText(after.intValue() + " g");
+    }
+
+    @FXML
+    public void setFish(ObservableValue<Number> ovn, Number before, Number after) {
+        fishSliderValue.setText(after.intValue() + " g");
+    }
+
+    @FXML
+    public void setCheese(ObservableValue<Number> ovn, Number before, Number after) {
+        cheeseSliderValue.setText(after.intValue() + " g");
+    }
+
+    @FXML
+    public void setRice(ObservableValue<Number> ovn, Number before, Number after) {
+        riceSliderValue.setText(after.intValue() + " g");
+    }
+    
+    @FXML
+    public void setEgg(ObservableValue<Number> ovn, Number before, Number after) {
+        eggSliderValue.setText(after.intValue() + " kpl");
+    }
+    
+    @FXML
+    public void setRestaurant(ObservableValue<Number> ovn, Number before, Number after) {
+        restaurantSliderValue.setText(after.intValue() + " kpl");
+    }
 
 }
