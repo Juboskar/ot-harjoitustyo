9e1a4cd69c98e08a8a03eabc89f5a8cc9ec2db41
Oskari-Juvakka <oskari.juvakka@gmail.com>
2020-11-27 18:23:59 +0200

    piilotettu toistaiseksi nappi joka ei tee mitään

--- Paastopaivakirja/src/main/java/com/example/paastopaivakirja/ui/HomeSceneController.java
+++ Paastopaivakirja/src/main/java/com/example/paastopaivakirja/ui/HomeSceneController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,87 +25,90 @@ import org.springframework.stereotype.Component;
 @Component
 @FxmlView("/fxml/HomeScene.fxml")
 public class HomeSceneController {
-
+    
     @Autowired
     SummaryService summaryService;
-
+    
     @Autowired
     LoginService loginService;
-
+    
     @Autowired
     EmissionService emissionService;
-
+    
     @Autowired
     FoodService foodService;
-
+    
     @Autowired
     TrafficService trafficService;
-
+    
     @Autowired
     ConsumptionService consumptionService;
-
+    
     @Autowired
     MainController main;
-
+    
     @FXML
     Text name;
-
+    
     @FXML
     Text yearlyTotal;
-
+    
     @FXML
     Text todaysTotal;
-
+    
     @FXML
     Text startDate;
-
+    
     @FXML
     Text foodCheck;
-
+    
     @FXML
     Text trafficCheck;
-
+    
     @FXML
     Text consumptionCheck;
-
+    
     @FXML
     Text summary;
-
+    
     @FXML
     Text total;
-
+    
+    @FXML/**/
+    Button button;
+    
     @FXML
     public void logOut(ActionEvent event) {
         main.showLoginScene();
     }
-
+    
     @FXML
     public void calculateYearlyEmissions(ActionEvent event) {
         main.showYearlyEmissionScene();
     }
-
+    
     @FXML
     public void calculateTodaysFood(ActionEvent event) {
         main.showFoodEmissionScene();
     }
-
+    
     @FXML
     public void calculateTodaysTraffic(ActionEvent event) {
         main.showTrafficEmissionScene();
     }
-
+    
     @FXML
     public void calculateTodaysConsumption(ActionEvent event) {
         main.showConsumptionScene();
     }
-
+    
     @FXML
     public void fillDays(ActionEvent event) {
         /*TODO: avaa näkymän jossa lista täyttämättömistä päivistä, 
         kun päivän valitsee, näkyy täyttämättömät kohdat 
         (ruoka/liikenne/kulutus)*/
     }
-
+    
     @FXML
     public void initialize() {
         String user = loginService.getCurrentUser();
@@ -127,5 +131,7 @@ public class HomeSceneController {
         foodCheck.setVisible(foodService.checkIfExists(user, date));
         trafficCheck.setVisible(trafficService.checkIfExists(user, date));
         consumptionCheck.setVisible(consumptionService.checkIfExists(user, date));
+        
+        button.setVisible(false);
     }
 }
